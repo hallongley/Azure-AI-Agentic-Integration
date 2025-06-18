5111fdcf9a21f318fa710d653caab90b6fbd01b6
Halliwell Mark Longley <128457565+hallongley@users.noreply.github.com>
2025-06-18 15:59:24 -0700

    Add files via upload

---
+++ Python_test2.py
@@ -0,0 +1,12 @@
+age = 65
+price   = 19.99
+first_name = "Josh"
+is_online = True
+print(age,price,first_name,is_online)
+
+input("Press Enter to continue...")
+print("Hello " + first_name + ", you are " + str(age) + " years old and the price is $" + str(price) + ". Online status: " + str(is_online))
+
+birth_year = 2025 - age     
+print("You were born in " + str(birth_year) + ".")
+
